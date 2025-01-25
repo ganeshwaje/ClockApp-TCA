8bad36890886b2509d28a964feb3400fbc0821c1
Ganesh Waje <waje.ganesh30@gmail.com>
2025-01-25 11:27:58 +0530

    initial commit

---
+++ ClockApp-TCA/ClockApp_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp_TCAApp.swift
+//  ClockApp-TCA
+//
+//  Created by Ganesh Waje on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
